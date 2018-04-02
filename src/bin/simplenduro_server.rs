79f1c80205eaa03d98f84dc501e0dc3cd6d07e80
Joel de Vahl <joel@devahl.com>
2018-04-02 09:17:27 +0200

    Started migration to diesel

--- src/bin/simplenduro_server.rs
+++ src/bin/simplenduro_server.rs
@@ -1,36 +1,114 @@
-extern crate futures;
-extern crate hyper;
-extern crate gotham;
-extern crate mime;
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
 
-use hyper::server::Http;
-use hyper::{Request, Response, StatusCode};
+#[macro_use]
+extern crate lazy_static;
+extern crate dotenv;
+extern crate rocket;
+#[macro_use]
+extern crate diesel;
+extern crate r2d2;
+extern crate r2d2_diesel;
 
-use gotham::http::response::create_response;
-use gotham::state::State;
-use gotham::handler::NewHandlerService;
+extern crate simplenduro;
 
-pub fn say_hello(state: State, _req: Request) -> (State, Response) {
-    let res = create_response(
-        &state,
-        StatusCode::Ok,
-        Some((
-            String::from("Hello World!").into_bytes(),
-            mime::TEXT_PLAIN,
-        )),
-    );
+use dotenv::dotenv;
+use std::env;
+use std::fmt;
+use std::fmt::Write;
+use rocket::request::{Outcome, FromRequest};
+use rocket::Outcome::{Success, Failure};
+use rocket::http::Status;
+use rocket::Request;
+use diesel::PgConnection;
+use diesel::prelude::*;
+use r2d2::{Pool, PooledConnection};
+use r2d2_diesel::ConnectionManager;
 
-    (state, res)
+use simplenduro::models::*;
+
+pub fn create_db_pool() -> Pool<ConnectionManager<PgConnection>> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").unwrap();
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    Pool::<ConnectionManager<PgConnection>>::new(manager).unwrap()
 }
 
-pub fn main() {
-    let addr = "127.0.0.1:7878".parse().unwrap();
+lazy_static! {
+    pub static ref DB_POOL: Pool<ConnectionManager<PgConnection>> = create_db_pool();
+}
 
-    let server = Http::new()
-        .bind(&addr, NewHandlerService::new(|| Ok(say_hello)))
-        .unwrap();
+pub struct DB(PooledConnection<ConnectionManager<PgConnection>>);
 
-    println!("Listening on http://{}", server.local_addr().unwrap());
+impl DB {
+    pub fn conn(&self) -> &PgConnection {
+        &*self.0
+    }
+}
 
-    server.run().unwrap();
+impl<'a, 'r> FromRequest<'a, 'r> for DB {
+    type Error = r2d2::Error;
+    fn from_request(_: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        match DB_POOL.get() {
+            Ok(conn) => Success(DB(conn)),
+            Err(e) => Failure((Status::InternalServerError, e)),
+        }
+    }
+}
+
+fn load_user(conn: &PgConnection, uid:i64) -> Option<User> {
+    use simplenduro::schema::users::dsl::*;
+    users
+        .filter(id.eq(uid))
+        .limit(1)
+        .load::<User>(conn)
+        .expect("Error loading user")
+        .pop()
+}
+
+#[get("/user/<uid>")]
+fn user(db: DB, uid: i64) -> String {
+    let conn = db.conn();
+    match load_user(conn, uid) {
+        None => "".to_string(),
+        Some(user) => format!("{}\n", user.name)
+    }
+}
+
+#[get("/event/<eid>")]
+fn event(db: DB, eid: i64) -> String {
+    use simplenduro::schema::events::dsl::*;
+    use simplenduro::schema::participations::dsl::*;
+
+    let conn = db.conn();
+    let event_rows = events
+        .filter(simplenduro::schema::events::dsl::id.eq(eid))
+        .limit(1)
+        .load::<Event>(conn)
+        .expect("Error loading event");
+    let participation_rows = participations
+        .filter(event_id.eq(eid))
+        .filter(total_elapsed_seconds.gt(0))
+        .load::<Participation>(conn)
+        .expect("Error loading participations");
+
+    let mut results = format!("Results for {}:\n", event_rows[0].name);
+    for (i, participation) in participation_rows.iter().enumerate() {
+        let username = match load_user(conn, participation.user_id) {
+            None => "Anonymous".to_string(),
+            Some(user) => user.name
+        };
+        let seconds: i64 = participation.total_elapsed_seconds;
+        
+        write!(&mut results, "{} {} - {} seconds\n", i + 1, username, seconds);
+    }
+
+    return results;
+}
+
+fn main() {
+    dotenv().ok();
+
+    rocket::ignite().mount("/", routes![user, event]).launch();
 }
