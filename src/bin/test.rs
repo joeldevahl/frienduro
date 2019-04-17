869a35973d05895b128ccd8a9dd80800b2a4cad2
Joel de Vahl <joel@devahl.com>
2019-04-17 17:57:09 +0200

    Fixed GPX loading. Merge sequence of lines in multi lines when matching segments.

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,13 +1,13 @@
 extern crate frienduro;
 extern crate getopts;
-extern crate postgres;
 extern crate postgis;
+extern crate postgres;
 
 use self::frienduro::*;
 use getopts::Options;
 use std::env;
-use std::path::Path;
 use std::fs;
+use std::path::Path;
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [options]", program);
@@ -39,24 +39,29 @@ fn main() {
 
         let segments_path = event_path.join("segments");
         let segment_files = fs::read_dir(segments_path).unwrap();
-        let segment_ids = segment_files.into_iter().map(|f| {
-            let segment_file = f.unwrap();
-            let segment_file_name = segment_file.file_name();
-            let segment_name = segment_file_name.to_str().unwrap();
-            println!("\tadding segment: {}", segment_name);
+        let segment_ids = segment_files
+            .into_iter()
+            .map(|f| {
+                let segment_file = f.unwrap();
+                let segment_file_name = segment_file.file_name();
+                let segment_name = segment_file_name.to_str().unwrap();
+                println!("\tadding segment: {}", segment_name);
 
-            let path = segment_file.path();
-            let filename = path.to_str().unwrap();
-            let gpx_data = read_whole_file(filename).unwrap();
-            let source_id = create_source_route(&db, &gpx_data);
+                let path = segment_file.path();
+                let filename = path.to_str().unwrap();
+                let gpx_data = read_whole_file(filename).unwrap();
+                let source_id = create_source_route(&db, &gpx_data);
 
-            let gpx = read_gpx(&gpx_data).unwrap();
-            let points = gpx.waypoints;
-            let route_id = get_new_route_id(&db);
-            store_points(&db, route_id, &points);
+                let gpx = read_gpx(&gpx_data).unwrap();
+                let track = &gpx.tracks[0];
+                let segment = &track.segments[0];
+                let points = &segment.points;
+                let route_id = get_new_route_id(&db);
+                store_points(&db, route_id, &points);
 
-            create_segment(&db, segment_name, route_id, source_id)
-        }).collect::<Vec<i64>>();
+                create_segment(&db, segment_name, route_id, source_id)
+            })
+            .collect::<Vec<i64>>();
         println!();
 
         let event_id = create_event(&db, event_name, &segment_ids);
@@ -65,18 +70,23 @@ fn main() {
         let users = fs::read_dir(users_path).unwrap();
         for u in users {
             let user_path = u.unwrap().path();
-            let user_name = user_path.file_stem().unwrap().to_str().unwrap();
-            let user_id = create_user(&db, user_name, "");
-            println!("\tadding user: {}", user_name);
+            let ext = user_path.extension().unwrap();
+            if ext == "gpx" {
+                let user_name = user_path.file_stem().unwrap().to_str().unwrap();
+                let user_id = create_user(&db, user_name, "");
+                println!("\tadding user: {}", user_name);
 
-            let filename = user_path.to_str().unwrap();
-            let gpx_data = read_whole_file(filename).unwrap();
-            let source_id = create_source_route(&db, &gpx_data);
-            let gpx = read_gpx(&gpx_data).unwrap();
-            let points = gpx.waypoints;
-            let route_id = get_new_route_id(&db);
-            store_points(&db, route_id, &points);
-            create_participation(&db, event_id, user_id, route_id, source_id);
+                let filename = user_path.to_str().unwrap();
+                let gpx_data = read_whole_file(filename).unwrap();
+                let source_id = create_source_route(&db, &gpx_data);
+                let gpx = read_gpx(&gpx_data).unwrap();
+                let track = &gpx.tracks[0];
+                let segment = &track.segments[0];
+                let points = &segment.points;
+                let route_id = get_new_route_id(&db);
+                store_points(&db, route_id, &points);
+                create_participation(&db, event_id, user_id, route_id, source_id);
+            }
         }
         println!();
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
+extern crate chrono;
 extern crate dotenv;
 extern crate gpx;
-extern crate postgres;
 extern crate postgis;
-extern crate chrono;
+extern crate postgres;
 
+use chrono::prelude::*;
 use dotenv::dotenv;
 use std::env;
-use chrono::prelude::*;
 
-use postgres::{Connection, TlsMode};
 use postgis::ewkb;
+use postgres::{Connection, TlsMode};
 
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
 
 pub fn read_whole_file(path: &str) -> Result<String, std::io::Error> {
     let mut file = File::open(path)?;
@@ -51,18 +51,22 @@ pub fn empty_db(db: &Connection) -> Result<(), postgres::Error> {
 }
 
 pub fn create_user(db: &Connection, name: &str, email: &str) -> i64 {
-    let rows = db.query(
-        "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
-        &[&name, &email],
-    ).unwrap();
+    let rows = db
+        .query(
+            "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
+            &[&name, &email],
+        )
+        .unwrap();
     rows.get(0).get(0)
 }
 
 pub fn create_source_route(db: &Connection, gpx_data: &str) -> i64 {
-    let rows = db.query(
-        "INSERT INTO source_routes (gpx) VALUES (XMLPARSE (DOCUMENT $1)) RETURNING id",
-        &[&gpx_data],
-    ).unwrap();
+    let rows = db
+        .query(
+            "INSERT INTO source_routes (gpx) VALUES (XMLPARSE (DOCUMENT $1)) RETURNING id",
+            &[&gpx_data],
+        )
+        .unwrap();
     rows.get(0).get(0)
 }
 
@@ -80,15 +84,19 @@ pub fn store_points(db: &Connection, route_id: i64, points: &[gpx::Waypoint]) {
         };
         db.execute(
             "INSERT INTO points (geom, route_id, ts, ele) VALUES ($1, $2, $3, $4)",
-            &[&point, &route_id, &p .time.unwrap(), &p.elevation.unwrap()],
-        ).unwrap();
+            &[&point, &route_id, &p.time.unwrap(), &p.elevation.unwrap()],
+        )
+        .unwrap();
     }
 }
 
 pub fn create_segment(db: &Connection, name: &str, route_id: i64, source_id: i64) -> i64 {
-    let rows = db.query("INSERT INTO segments (name, route_id, source_id) VALUES ($1, $2, $3) RETURNING id",
-        &[&name, &route_id, &source_id]
-    ).unwrap();
+    let rows = db
+        .query(
+            "INSERT INTO segments (name, route_id, source_id) VALUES ($1, $2, $3) RETURNING id",
+            &[&name, &route_id, &source_id],
+        )
+        .unwrap();
     let segment_id: i64 = rows.get(0).get(0);
 
     db.execute(
@@ -101,18 +109,21 @@ pub fn create_segment(db: &Connection, name: &str, route_id: i64, source_id: i64
     segment_id
 }
 
-pub fn create_event(db: &Connection, name: &str, segment_ids: &[i64]) -> i64{
-    let rows = db.query(
-        "INSERT INTO events (name) VALUES ($1) RETURNING id",
-        &[&name],
-    ).unwrap();
+pub fn create_event(db: &Connection, name: &str, segment_ids: &[i64]) -> i64 {
+    let rows = db
+        .query(
+            "INSERT INTO events (name) VALUES ($1) RETURNING id",
+            &[&name],
+        )
+        .unwrap();
     let event_id: i64 = rows.get(0).get(0);
 
     for segment_id in segment_ids {
         db.execute(
             "INSERT INTO event_segments (event_id, segment_id) VALUES ($1, $2)",
             &[&event_id, &segment_id],
-        ).unwrap();
+        )
+        .unwrap();
     }
 
     event_id
@@ -129,71 +140,170 @@ struct SegmentInfo {
 
 fn interp_point(db: &postgres::Connection, route_id: i64, point: &ewkb::Point) -> DateTime<Utc> {
     // TODO: assumes we only passes once around the segment
-    let rows = db.query(
-        "SELECT ts
+    let rows = db
+        .query(
+            "SELECT ts
          FROM points
          WHERE route_id = $1
          ORDER BY ST_Distance(geom, $2) ASC
          LIMIT 1",
-        &[&route_id, &point],
-    ).unwrap();
+            &[&route_id, &point],
+        )
+        .unwrap();
     return rows.get(0).get(0);
 }
 
-fn match_segment(
+fn match_segments(
     db: &postgres::Connection,
-    ls: ewkb::LineString,
+    lines: &Vec<ewkb::LineString>,
     segment_start: &ewkb::Point,
     segment_end: &ewkb::Point,
     pid: i64,
     rid: i64,
     sid: i64,
 ) -> Option<chrono::Duration> {
-    let points = &ls.points;
-    let start = &points[0];
-    let end = &(points.last().unwrap());
+    // We try to go over all lines in sequence and build a chain of acceptable lines
+    // if we can do that we can find a time starting in one line and ending in another
 
-    let distance_rows = db.query(
-        "SELECT
-        ST_Distance($1::geography, $2::geography) AS dist_start,
-        ST_Distance($3::geography, $4::geography) AS dist_end",
-        &[&segment_start, &start, &segment_end, &end],
-    ).unwrap();
+    // TODO: handle the case where the segment is hit multiple times, now we only take the first time
 
-    let distance_start: f64 = distance_rows.get(0).get(0);
-    let distance_end: f64 = distance_rows.get(0).get(1);
-    if distance_start < 20.0 && distance_end < 20.0 {
-        let start_time = interp_point(&db, rid, start);
-        let end_time = interp_point(&db, rid, end);
-        let diff = end_time.signed_duration_since(start_time);
+    // No lines, no match
+    if lines.len() == 0 {
+        return None;
+    }
 
-        if diff >= chrono::Duration::seconds(0) {
-            let seconds: i64 = diff.num_seconds();
-            db.execute(
-                "INSERT INTO participation_segments (participation_id, segment_id, elapsed_seconds, geom) VALUES ($1, $2, $3, $4)",
-                &[&pid, &sid, &seconds, &ls],
-            ).unwrap();
+    let mut total_time: i64 = 0;
+    let mut start_line_index = 0;
+    let mut last_end = ewkb::Point {
+        x: 0.0,
+        y: 0.0,
+        srid: None,
+    };
 
-            return Some(diff);
+    // Find suitable start point in a segment
+    loop {
+        let line = &lines[start_line_index];
+        let points = &line.points;
+        let start = &points[0];
+        let end = points.last().unwrap();
+
+        let distance_rows = db
+            .query(
+                "SELECT
+                        ST_Distance($1::geography, $2::geography) AS dist_start,
+                        ST_Distance($3::geography, $4::geography) AS dist_end",
+                &[&segment_start, &start, &segment_end, end],
+            )
+            .unwrap();
+
+        let distance_start: f64 = distance_rows.get(0).get(0);
+
+        if distance_start < 20.0 {
+            let start_time = interp_point(&db, rid, start);
+            let end_time = interp_point(&db, rid, end);
+            let diff = end_time.signed_duration_since(start_time);
+            total_time += diff.num_seconds();
+
+            // if end distance also matches here we are done!
+            let distance_end: f64 = distance_rows.get(0).get(1);
+            if distance_end < 20.0 {
+                db.execute(
+                    "INSERT INTO participation_segments (participation_id, segment_id, elapsed_seconds, geom) VALUES ($1, $2, $3, $4)",
+                    &[&pid, &sid, &total_time, &line],
+                ).unwrap();
+
+                return Some(diff);
+            }
+
+            // This segment matched a start but not the end
+            // we store the initial point and try to chain it to the next
+            last_end = end.clone();
+            break;
+        }
+
+        start_line_index += 1;
+        if start_line_index >= lines.len() {
+            return None;
         }
     }
 
-    return None;
+    let mut end_line_index = start_line_index + 1;
+
+    // start match was last line, no match
+    if end_line_index == lines.len() {
+        return None;
+    }
+
+    loop {
+        let line = &lines[end_line_index];
+        let points = &line.points;
+        let start = &points[0];
+        let end = points.last().unwrap();
+
+        let distance_rows = db
+            .query(
+                "SELECT
+                        ST_Distance($1::geography, $2::geography) AS dist_start,
+                        ST_Distance($3::geography, $4::geography) AS dist_end",
+                &[&last_end, &start, &segment_end, end],
+            )
+            .unwrap();
+
+        let distance_start: f64 = distance_rows.get(0).get(0);
+
+        // current line did not connect with previous
+        // no match for now
+        // TODO: restart searching for a match from the next line as start
+        if distance_start >= 20.0 {
+            return None;
+        }
+
+        let start_time = interp_point(&db, rid, start);
+        let end_time = interp_point(&db, rid, end);
+        let diff = end_time.signed_duration_since(start_time);
+        total_time += diff.num_seconds();
+
+        // does this line complete the segment?
+        let distance_end: f64 = distance_rows.get(0).get(1);
+        if distance_end < 20.0 {
+            if diff >= chrono::Duration::seconds(0) {
+                db.execute(
+                    "INSERT INTO participation_segments (participation_id, segment_id, elapsed_seconds, geom) VALUES ($1, $2, $3, $4)",
+                    &[&pid, &sid, &total_time, &line],
+                ).unwrap();
+
+                return Some(diff);
+            }
+        }
+
+        end_line_index += 1;
+        if end_line_index >= lines.len() {
+            return None;
+        }
+
+        last_end = end.clone();
+    }
 }
 
 fn update_participation_timing(db: &Connection, participation_id: i64) {
     // TODO: to this whole thing in the DB
-    let participation_rows = db.query("SELECT * FROM participations WHERE id = $1", &[&participation_id])
+    let participation_rows = db
+        .query(
+            "SELECT * FROM participations WHERE id = $1",
+            &[&participation_id],
+        )
         .unwrap();
 
     let route_id: i64 = participation_rows.get(0).get("route_id");
     let event_id: i64 = participation_rows.get(0).get("event_id");
 
     // TODO: handle the case where the user submits many atempts on a single event
-    let count_rows = db.query(
-        "SELECT COUNT(segment_id) FROM participation_segments WHERE participation_id = $1",
-        &[&participation_id],
-    ).unwrap();
+    let count_rows = db
+        .query(
+            "SELECT COUNT(segment_id) FROM participation_segments WHERE participation_id = $1",
+            &[&participation_id],
+        )
+        .unwrap();
     let old_count: i64 = count_rows.get(0).get(0);
     if old_count > 0 {
         println!(
@@ -243,21 +353,40 @@ fn update_participation_timing(db: &Connection, participation_id: i64) {
         match is_mls {
             None => (),
             Some(Ok(mls)) => {
-                for ls in mls.lines {
-                    match match_segment(&db, ls, &segment_start, &segment_end, participation_id, route_id, segment_id) {
-                        Some(seconds) => {
-                            let segment_match = SegmentMatch { elapsed: seconds.num_seconds() };
-                            segment_info.matches.push(segment_match);
-                        }
-                        None => (),
+                match match_segments(
+                    &db,
+                    &mls.lines,
+                    &segment_start,
+                    &segment_end,
+                    participation_id,
+                    route_id,
+                    segment_id,
+                ) {
+                    Some(seconds) => {
+                        let segment_match = SegmentMatch {
+                            elapsed: seconds.num_seconds(),
+                        };
+                        segment_info.matches.push(segment_match);
                     }
+                    None => (),
                 }
             }
             Some(Err(..)) => {
                 let ls: ewkb::LineString = matched_rows.get(0).get("cut");
-                match match_segment(&db, ls, &segment_start, &segment_end, participation_id, route_id, segment_id) {
+                let lines = vec![ls];
+                match match_segments(
+                    &db,
+                    &lines,
+                    &segment_start,
+                    &segment_end,
+                    participation_id,
+                    route_id,
+                    segment_id,
+                ) {
                     Some(seconds) => {
-                        let segment_match = SegmentMatch { elapsed: seconds.num_seconds() };
+                        let segment_match = SegmentMatch {
+                            elapsed: seconds.num_seconds(),
+                        };
                         segment_info.matches.push(segment_match);
                     }
                     None => (),
@@ -293,11 +422,18 @@ fn update_participation_timing(db: &Connection, participation_id: i64) {
             "UPDATE participations SET total_elapsed_seconds = $1
             WHERE id = $2",
             &[&total_elapsed, &participation_id],
-        ).unwrap();
+        )
+        .unwrap();
     }
 }
 
-pub fn create_participation(db: &Connection, event_id: i64, user_id: i64, route_id: i64, source_id: i64) -> i64 {
+pub fn create_participation(
+    db: &Connection,
+    event_id: i64,
+    user_id: i64,
+    route_id: i64,
+    source_id: i64,
+) -> i64 {
     let rows = db.query("INSERT INTO participations (event_id, user_id, route_id, source_id) VALUES ($1, $2, $3, $4) RETURNING id",
         &[&event_id, &user_id, &route_id, &source_id]
     ).unwrap();
@@ -326,14 +462,17 @@ pub fn get_event_results(db: &Connection, event_id: i64) -> Vec<EventResult> {
         &[&event_id],
     ).unwrap();
 
-    event_rows.iter().map(|row| {
-        let username: String = row.get("name");
-        let maybe_elapsed: Option<postgres::Result<i64>> = row.get_opt("total_elapsed_seconds");
-        let time = match maybe_elapsed {
-            Some(Ok(elapsed)) => elapsed,
-            Some(Err(..)) | None => 0,
-        };
+    event_rows
+        .iter()
+        .map(|row| {
+            let username: String = row.get("name");
+            let maybe_elapsed: Option<postgres::Result<i64>> = row.get_opt("total_elapsed_seconds");
+            let time = match maybe_elapsed {
+                Some(Ok(elapsed)) => elapsed,
+                Some(Err(..)) | None => 0,
+            };
 
-        EventResult { username, time }
-    }).collect()
+            EventResult { username, time }
+        })
+        .collect()
 }
