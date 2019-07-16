3562788524324c86cb29adc058700803dd85a7d6
Joel de Vahl <joel@devahl.com>
2019-07-16 12:09:12 +0200

    Upgraded actix

--- src/bin/friendurod.rs
+++ src/bin/friendurod.rs
@@ -1,7 +1,6 @@
-#[macro_use]
-extern crate lazy_static;
 extern crate actix_web;
 extern crate dotenv;
+extern crate futures;
 extern crate postgres;
 extern crate r2d2;
 extern crate r2d2_postgres;
@@ -13,41 +12,37 @@ use std::env;
 use std::fmt::Write;
 
 use actix_web::http::Method;
-use actix_web::{server, App, HttpRequest, HttpResponse};
+use actix_web::{get, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use frienduro::{get_event, get_user};
+use futures::Future;
 use r2d2::Pool;
 use r2d2_postgres::{PostgresConnectionManager, TlsMode};
 
-pub fn create_db_pool() -> Pool<PostgresConnectionManager> {
-    dotenv().ok();
+#[get("/user/{id}")]
+fn user(
+    req: HttpRequest,
+    db: web::Data<Pool<PostgresConnectionManager>>,
+    id: web::Path<i64>,
+) -> HttpResponse {
+    let conn = db.get().unwrap();
 
-    let database_url = env::var("DATABASE_URL").unwrap();
-    let manager = PostgresConnectionManager::new(database_url, TlsMode::None).unwrap();
-    Pool::<PostgresConnectionManager>::new(manager).unwrap()
+    let uid = id.into_inner();
+    let user = get_user(&conn, uid).unwrap();
+
+    HttpResponse::Ok().json(user);
 }
 
-lazy_static! {
-    pub static ref DB_POOL: Pool<PostgresConnectionManager> = create_db_pool();
-}
+#[get("/event/{id}")]
+fn event(
+    req: HttpRequest,
+    db: web::Data<Pool<PostgresConnectionManager>>,
+    id: web::Path<i64>,
+) -> HttpResponse {
+    let conn = db.get().unwrap();
 
-fn user(req: &HttpRequest) -> HttpResponse {
-    let db = DB_POOL.get().unwrap();
+    let eid = id.into_inner();
 
-    let uid = req.match_info().get("id").unwrap().parse::<i64>().unwrap();
-
-    let user = get_user(&db, uid).unwrap();
-
-    let result: String = user.name;
-
-    HttpResponse::Ok().content_type("text/plain").body(result)
-}
-
-fn event(req: &HttpRequest) -> HttpResponse {
-    let db = DB_POOL.get().unwrap();
-
-    let eid = req.match_info().get("id").unwrap().parse::<i64>().unwrap();
-
-    let event = get_event(&db, eid).unwrap();
+    let event = get_event(&conn, eid).unwrap();
 
     let mut results = format!("Results for {}:\n", event.name);
     for (i, r) in event.results.iter().enumerate() {
@@ -65,12 +60,19 @@ fn event(req: &HttpRequest) -> HttpResponse {
 }
 
 fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
     dotenv().ok();
 
-    server::new(|| {
+    let database_url = env::var("DATABASE_URL").unwrap();
+    let manager = PostgresConnectionManager::new(database_url, TlsMode::None).unwrap();
+    let pool = Pool::<PostgresConnectionManager>::new(manager).unwrap();
+
+    HttpServer::new(move || {
         App::new()
-            .resource("/user/{id}", |r| r.method(Method::GET).f(user))
-            .resource("/event/{id}", |r| r.method(Method::GET).f(event))
+            .wrap(middleware::Logger::default())
+            .data(pool.clone())
+            .service(user)
+            .service(event)
     })
     .bind("127.0.0.1:8088")
     .unwrap()
